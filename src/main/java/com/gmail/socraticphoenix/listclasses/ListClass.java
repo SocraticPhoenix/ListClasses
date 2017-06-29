56b88181ae9038fd291e33bfc958e749542bdbc7
John-Ryan Dunn <John-ryan.Dunn@jax.org>
2017-06-29 13:46:07 -0400

    fix ListClass

--- src/main/java/com/gmail/socraticphoenix/listclasses/ListClass.java
+++ src/main/java/com/gmail/socraticphoenix/listclasses/ListClass.java
@@ -1,5 +1,12 @@
 package com.gmail.socraticphoenix.listclasses;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
 public @interface ListClass {
 
     boolean ignore() default false;
